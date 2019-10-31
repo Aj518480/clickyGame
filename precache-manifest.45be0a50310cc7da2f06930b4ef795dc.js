e449075909efa33cfb7717864993705e9f96a094
aj518480 <allisoncordovez@gmail.com>
2019-10-31 15:05:08 -0400

    Updates

---
+++ precache-manifest.45be0a50310cc7da2f06930b4ef795dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f6fb4fe75b88bab907b57e384a2fbe",
+    "url": "/clickyGame/./index.html"
+  },
+  {
+    "revision": "d06bb3996a3af71fa1e2",
+    "url": "/clickyGame/./static/css/main.950c7132.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/clickyGame/./static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "d06bb3996a3af71fa1e2",
+    "url": "/clickyGame/./static/js/main.7b5dab1c.chunk.js"
+  },
+  {
+    "revision": "29f03677da62b0a0328a",
+    "url": "/clickyGame/./static/js/runtime-main.cd8934be.js"
+  }
+]);
