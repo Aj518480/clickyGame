29e85ec1cf76b6a02ee6aef115006b7596ed1031
aj518480 <allisoncordovez@gmail.com>
2019-11-01 13:07:07 -0400

    added correct logic

--- clicky-game/src/components/Game/index.js
+++ clicky-game/src/components/Game/index.js
@@ -2,72 +2,69 @@ import React, { Component } from "react";
 import Wrapper from "../Wrapper";
 import Title from "../Title";
 import CharacterCard from "../CharacterCard";
-import character from "../../characters.json";
+import characterFromJson from "../../characters.json";
 import NavBar from "../NavBar";
 
 
 class Game extends Component{
-    constructor(props){
-        super(props);
+    // constructor(props){
+    //     super(props);
 
         //These will be the starting state for these variables until changed as the game goes on
-        this.state = {
+        state = {
             score: 0,
             topScore: 0,
-            character,
-        };
+            character: characterFromJson,
+            tempCharacter: characterFromJson
+        }
 
         //this function handles the clicked and unclicked state of the characters
         //it passes conditionals whether character was clicked or not
-    
-        handleClick = character => {
-            
-            for (var i = 0; i < character.length; i++) {
-              var characters = character[i]
-              // If character is clicked it will console.log(game over) and check the state of the score and top score
-                if(characters.clicked===true){
-                    console.log("game over")
-                    if(this.state.score>this.state.topScore){
-                        this.setState({topScore:this.state.score})
-                        this.setState({score:0})
-                    }
-                }
-                //If character has not been clicked the game will continue and shuffle after every click with
-                // the shuffle function built on the next function.
-                else if(characters.clicked===false){
-                    this.setState({score:this.state.score+1})
-                    shuffle(character)
-                }
-              }
+
+        // eslint-disable-next-line
+        handleClick = id => {
+            let arrayOfIds = this.state.tempCharacter.map(item => (item.id))
+            console.log(arrayOfIds)
+            if (arrayOfIds.indexOf(id) === -1){
+                console.log("you lost")
+                this.setState({
+                    tempCharacter: characterFromJson
+                })
+            } else {
+                console.log("keep playing");
+                arrayOfIds = this.state.tempCharacter.filter(item => item.id !== id);
+                console.log(arrayOfIds)
+                this.setState({
+                    tempCharacter: arrayOfIds
+                })
+                this.shuffle(characterFromJson)
+            }
+            console.log(this.state.tempCharacter);
               
             };
 
     
 
         //This is will be randomizing the images everytime a click of a image is made.
-          let shuffle = character =>{
-             let i = character.length
-             while(i>1){
-            
-           let randomNumber=Math.floor(Math.random(i+1))
-           const temp = character[i];
-            character[i] = character[randomNumber]
-            character[randomNumber] = temp;
-            i--;
-            
-             
-         }
-         return character;
+          shuffle = a =>{
+           
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        this.setState({
+            character: a
+        });
          };
          
         
       
 
     // this will be the information we will be rendering for the user to see.
-
+    render(){
     return (
         <Wrapper>
-            <NavBar>
+            
                 <Title>Mario's Clicky Game</Title>
                 {this.state.character.map(character => (
                     <CharacterCard
@@ -78,14 +75,13 @@ class Game extends Component{
                     />
 
                 ))};
-      </NavBar>
+            
         </Wrapper>
-
-
-
         );
     }
+// }
 }
+
 
 
 export default Game
